608a84586b0c7d44d5f856f509a1e03886ec0071
Ragib Shariar <srragib@gmail.com>
2024-06-28 01:27:45 +0600

    add typescript setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => { 
+  res.send("hello world!!!")
+})
+
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
