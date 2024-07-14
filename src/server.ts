b0236ed5775984e88ca39f70545851d1ff114a78
Ragib Shariar <srragib@gmail.com>
2024-07-15 01:39:44 +0600

    add unhandledRejection to server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
+/* eslint-disable no-console */
+import { Server } from "http";
 import connectDB from "./DB";
 import app from "./app";
 import { config } from "./config";
 
 const port = config.port || 5000;
 
+let server: Server;
+
 connectDB()
   .then(() => {
-    app.listen(port, () => {
+    server = app.listen(port, () => {
       console.log(`server listening on ${port}`);
     });
   })
   .catch((err) => console.log("Failed to connect to database.", err));
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (reason, promise) => {
+  console.log(`😈 unhandledRejection is detected at ${promise}, ${reason}`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
